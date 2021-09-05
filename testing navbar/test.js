e55e4398bc992d7400b4d4e99b66738783f6cbf9
David Lopez <dmatthew2005@gmail.com>
2021-09-05 07:53:28 -0400

    A2

--- testing navbar/test.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.getElementsByClassName('toggleBtn')[0]
-const navbarLinks = document.getElementsByClassName('navbarLinks')[0]
-
-toggleBtn.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
