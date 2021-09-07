e135e6d4205803aaf449bd1eca2272e9ac5c5a05
David Lopez <dmatthew2005@gmail.com>
2021-09-06 23:46:46 -0400

    a2

--- a.js
+++ a.js
@@ -1,8 +1,8 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
+}
