2defa9904f2dc13e56dc9e8639a25968cfeee83f
David Lopez <dmatthew2005@gmail.com>
2021-09-06 22:39:42 -0400

    A2

--- testing navbar/test.js
+++
@@ -1,6 +0,0 @@
-const waffle = document.getElementsByClassName('waffle')[0]
-const navLinks = document.getElementsByClassName('navLinks')[0]
-
-waffle.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
-})
